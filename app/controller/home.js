066e6bb2f7d0b5fbd5fefaaa915a08bafef6088c
umbraci <umbraci@qq.com>
2022-05-30 18:28:55 +0800

    初始化mongoose配置

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,6 +5,11 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    const User = this.app.model.User;
+    await new User({
+      userName: 'test',
+      password: 'testPassword',
+    }).save();
     ctx.body = 'hi, egg';
   }
 }
