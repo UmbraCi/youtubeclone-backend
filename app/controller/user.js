a193a4cec28b4a827e5077b8d2b1edc1b6983c32
umbraci <umbraci@qq.com>
2022-06-06 18:38:35 +0800

    完成用户注册

--- app/controller/user.js
+++ app/controller/user.js
@@ -5,13 +5,34 @@ const Controller = require('egg').Controller;
 class UserController extends Controller {
   async create() {
     const { ctx } = this;
-    // 数据校验
+    const body = ctx.request.body;
+    // 1.数据校验
     ctx.validate({
       username: { type: 'string' },
       email: { type: 'email' },
       password: { type: 'string' },
     });
-    ctx.body = 'user create';
+
+    if (await this.service.user.findByUsername(body.username)) {
+      this.ctx.throw(422, '用户已存在');
+    }
+    if (await this.service.user.findByEmail(body.email)) {
+      this.ctx.throw(422, '邮箱已存在');
+    }
+
+    // 2.保存用户
+    const user = await this.service.user.createUser(body);
+    // 3.生成token
+    // 4.发送响应
+    ctx.body = {
+      user: {
+        email: user.email,
+        username: user.username,
+        // token:this.service.user.generateToken(user._id),
+        channelDescription: user.channelDescription,
+        avatar: user.avatar,
+      },
+    };
   }
 }
 
